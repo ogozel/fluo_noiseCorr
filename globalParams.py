27f59e01f42aed3abee4aac979777d26904e79ae
Olivia Gozel <olivia.gozel@gmail.com>
2022-11-07 11:25:44 -0500

    better comments

--- globalParams.py
+++ globalParams.py
@@ -8,21 +8,22 @@ Define global variables
 """
 
 # Path to data
-dataDir = 'C:\\Users\\olivi\\Dropbox\\Projects\\U19_project\\U19data\\'
-processedDataDir = 'C:\\Users\\olivi\\Dropbox\\Projects\\U19_project\\U19data_postprocessed\\'
+projectDir = 'C:\\Users\\olivi\\Dropbox\\Projects\\U19_project\\'
+dataDir = projectDir + 'U19data\\'
+processedDataDir = projectDir + 'U19data_postprocessed\\'
 
-# Fluorescence data
-ori = (45, 135, 180, 270) # grating orientations in degrees
-nOri = 4
+# Grating orientations in degrees
+ori = (45, 135, 180, 270)
+nOri = len(ori)
+# Name extension for neuropil subtraction
 neuropilSub = ('NpMethod0',\
                'NpMethod1_Coe0.25_Exclusion_NpSize30',\
                'NpMethod1_Coe0.5_Exclusion_NpSize30',\
                'NpMethod1_Coe0.75_Exclusion_NpSize30',\
                'NpMethod1_Coe1_Exclusion_NpSize30')
-
-# threshold_dff0 = 10 # [%]
-threshold_distance = 5 # [um] (to merge ROIs that are too close to each others)
-# NB: for Peijia: use threshold_distance = 2.5, then he asked 5
-threshold_pval_OS = 0.05 # pvalue below which a neuron is considered orientation selective
+# Minimal distance between pairs of ROIs in [um]
+threshold_distance = 5
+# p-value below which a neuron is considered orientation selective
+threshold_pval_OS = 0.05 
+# p-value below which a neuron is considered visually evoked
 threshold_pval_visuallyEvoked = 0.05
-# threshold_percentile = 0.15 # percentile for baselineRaw under which ROIs are fake
